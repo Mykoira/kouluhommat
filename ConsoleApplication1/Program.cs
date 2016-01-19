3122ef01a6ff1d4dd535d1d6215ceff6677624f9
Mykoira <leevi.paivarinta123@gmail.com>
2016-01-19 19:24:37 +0200

    Update Program.cs

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +9,95 @@ namespace ConsoleApplication1
     class Program
     {
         static void Main(string[] args)
-        {
-            int 22
+        {/*
+            int luku;
+            Console.Write("Anna luku ");
+            luku = int.Parse(Console.ReadLine());
+
+            switch (luku)
+            {
+                case 1:
+                    Console.WriteLine("Annoit luvun yksi");
+                    break;
+                case 2:
+                    Console.WriteLine("Annoit luvun kaksi");
+                    break;
+                case 3:
+                    Console.WriteLine("Annoit luvun kolme");
+                    break;
+                default: Console.WriteLine("Joku muu luku");
+                    break;
+            }*/
+            /*
+              int tulos;
+            Console.Write("Anna pistemäärä ");
+            tulos = int.Parse(Console.ReadLine());
+
+            switch (tulos)
+            {
+                case 0:
+                    Console.WriteLine("Koulunumero on 0");
+                    break;
+                case 1:
+                    Console.WriteLine("Koulunumero on 0");
+                    break;
+                case 2:
+                    Console.WriteLine("Koulunumero on 1");
+                    break;
+                case 3:
+                    Console.WriteLine("Koulunumero on 1");
+                    break;
+                case 4:
+                    Console.WriteLine("Koulunumero on 2");
+                    break;
+                case 5:
+                    Console.WriteLine("Koulunumero on 2");
+                    break;
+                case 6:
+                    Console.WriteLine("Koulunumero on 3");
+                    break;
+                case 7:
+                    Console.WriteLine("Koulunumero on 3");
+                    break;
+                case 8:
+                    Console.WriteLine("Koulunumero on 4");
+                    break;
+                case 9:
+                    Console.WriteLine("Koulunumero on 4");
+                    break;
+                case 10:
+                    Console.WriteLine("Koulunumero on 5");
+                    break;
+                case 11:
+                    Console.WriteLine("Koulunumero on 5");
+                    break;
+                case 12:
+                    Console.WriteLine("Koulunumero on 5");
+                    break;
+                default:
+                    Console.WriteLine("virheellinen tulos");
+                    break;
+            }*/
+             /* int tulos1;
+            int tulos2;
+            int tulos3;
+            int vastaus;
+            Console.Write("Anna numero ");
+            tulos1 = int.Parse(Console.ReadLine());
+            Console.Write("Anna numero ");
+            tulos2 = int.Parse(Console.ReadLine());
+            Console.Write("Anna numero ");
+            tulos3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(vastaus = (tulos1 + tulos2 + tulos3) / 3);
+            */
+            /*  int ika;
+            Console.Write("Anna Ikäsi ");
+            ika = int.Parse(Console.ReadLine());
+
+            if (ika > 65) Console.Write("Olet seniori ");
+            else if (ika < 18) Console.Write("Olet alaikäinen ");
+            else Console.Write("Olet täysi-ikäinen ");
+            */
         }
     }
 }
